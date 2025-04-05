dcb09db747e463eedfe202062865387d8e5cbd52
AAMLYT <3288465062@qq.com>
2025-04-05 15:25:34 +0800

    hh

--- 1-2 光敏传感器/Hardware/KEY.h
+++
@@ -1,8 +0,0 @@
-#ifndef __KEY_H
-#define __KEY_H
-
-void KEY_Init(void);
-uint16_t Get_KeyValue(uint16_t GPIO_Pinx);
-
-#endif
-
--- 1-2 光敏传感器/Hardware/LightSensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LightSensor_H
-#define	__LightSensor_H
-
-
-void LightSensor_Init(void);
-uint16_t Read_Ligh(void);
-
-#endif
-
