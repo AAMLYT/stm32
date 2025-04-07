fbd96c38dc017048d92d38cc92b06998d2b9b53c
AAMLYT <3288465062@qq.com>
2025-04-07 16:57:49 +0800

    3-2 旋转编码器计数

--- 3-1 外部中断/Hardware/DSSensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DSSensor_H
-#define __DSSensor_H
-
-void DsSensor_Init(void);
-uint16_t DS_count(void);
-
-#endif
---
+++ 3-2 旋转编码器计数/Hardware/Encoding.h
@@ -0,0 +1,7 @@
+#ifndef __ENCODING_H
+#define __ENCODING_H
+
+void Encode_Init(void);
+uint16_t RE_Num(void);
+
+#endif
